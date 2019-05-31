e05c4d267ac4c724606ac77ac470d62c6117de7a
kvellaidurai <46344094+kvellaidurai@users.noreply.github.com>
2019-05-31 10:43:23 +0530

    Update day-9-ex.py

--- day-9-ex.py
+++ day-9-ex.py
@@ -362,6 +362,22 @@ print(check_squares(number))
 
 
 
+#PF-Prac-45
+from difflib import SequenceMatcher
+def longest_common_substring(str1, str2):
+    match=SequenceMatcher(None,str1,str2).find_longest_match(0,len(str1) , 0,len(str2))
+    if(match.size!=0):
+        return(str1[match.a:match.a+match.size])
+    else:
+        return ("")
+        
+output=longest_common_substring("discatenation","concatenation")
+print("The longest overlap of characters between string1 and string2:",output)
+output1=longest_common_substring("assured","measured")
+print("The longest overlap of characters between string1 and string2:",output1)
+
+
+
 
 
 
